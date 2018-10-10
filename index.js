d8110943d3b1ee7f345ca4f2dd6a456d88c0dabe
Matt Doering <mattdoering86@gmail.com>
2018-10-10 18:31:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ for (var i = 0; i < line.length; i++) {
 return `Welcome, ${name}. You are number ${count} in line.`;
 }
 
+
+
+
+function currentLineMine(line) {
+
+if (line.length === 0) {
+ return 'The line is currently emtpy.'
+}
+
+var lineNumbersArray = [];
+var currentNum = 0;
+
+  for (var i = 0; i < line.length; i++) {
+   if (currentNum < line.length) {
+	currentNum++;
+   }
+lineNumbersArray.push(currentNum + "." + line[i]);
+  }
+
+return 'The line is currently: ' + lineNumbersArray.join(', ');
+
+}
