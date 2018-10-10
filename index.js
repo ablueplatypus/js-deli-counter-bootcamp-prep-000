fdbef2d59d6061018de82ff297c99da24dbedcb0
Matt Doering <mattdoering86@gmail.com>
2018-10-10 17:22:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function takeANumber(line, name) {
+
+var count = 0;
+line.push(name);
+
+for (var i = 0; i < line.length; i++) {
+  if (line.length > count) {
+     count++
+ }
+ 
+}
+
+return `Welcome, ${name}. You are number ${count} in line.`;
+}
